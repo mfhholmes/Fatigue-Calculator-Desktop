b7e544909c25282a2374cbb389c09db46862aa65
unknown <Doc@Doc2.(none)>
2012-05-12 20:26:35 +0800

    config changes working

--- Config/ConfigSettings.cs
+++ Config/ConfigSettings.cs
@@ -37,113 +37,25 @@ namespace Fatigue_Calculator_Desktop.Config
         }
         private static FatigueCalcConfig loadSettings()
         {
-            //have we got a source?
-            // if not grab the default source file
-            if (_source == null)
-                source = new ConfigSourceFile();
+            _settings = new FatigueCalcConfig();
             // have we got an XML?
             if (_settingsXML == null)
                 _settingsXML = source.getConfigXML();
-            // set the config settings
-            if (_settings == null)
-                _settings = new FatigueCalcConfig();
-            
 
             //TODO: check if the XML has a redirection to another source
-            if (_settingsXML.InnerXml.Length == 0)
+            if (_settingsXML.InnerXml.Length > 0)
             {
-                // can't populate from the xml because it's blank, so we need to create the default XML and then save that
-                if (createDefaultConfig())
-                    saveSettings();
-            }
-            else
-            // iterate the XML
-            foreach (XmlElement settingsNode in _settingsXML.DocumentElement.SelectNodes("//setting"))
-            {
-                // create config items
-                configItem newItem = new configItem();
-                if(newItem.parseXML(settingsNode))
-                    _settings.Add(newItem);
+                foreach (XmlElement settingsNode in _settingsXML.DocumentElement.SelectNodes("//setting"))
+                {
+                    // create config items
+                    configItem newItem = new configItem();
+                    if (newItem.parseXML(settingsNode))
+                        _settings.Add(newItem);
+                }
             }
             return _settings;
         }
 
-        private static bool createDefaultConfig()
-        {
-            _settings = new FatigueCalcConfig();
-            configItem setting;
-
-            //low threshold
-            setting = new configItem();
-            setting.validationXML = "<validation><type>int</type><min>1</min><max>24</max></validation>";
-            setting.key = "lowThreshold";
-            setting.name = "Low Threshold";
-            setting.description = "The calculation score that separates Moderate fatigue risk from High fatigue risk";
-            setting.intValue = 6;
-            _settings.Add(setting);
-
-            //high threshold
-            setting = new configItem();
-            setting.validationXML = "<validation><type>int</type><min>2</min><max>24</max></validation>";
-            setting.key = "highThreshold";
-            setting.name = "High Threshold";
-            setting.description = "The calculation score that separates High fatigue risk from Extreme fatigue risk";
-            setting.intValue = 12;
-            _settings.Add(setting);
-
-            //ID Lookup File
-            setting = new configItem();
-            setting.validationXML = "<validation><type>file</type><mustExist>no</mustExist></validation>";
-            setting.key = "IDLookupFile";
-            setting.name = "Identity Lookup File";
-            setting.description = "The file containing the list of valid identities for this calculator";
-            setting.strValue = "%appdata%/fatigue calculator/Identities.csv";
-            _settings.Add(setting);
-
-            //ID Lookup Type
-            setting = new configItem();
-            setting.validationXML = "<validation><type>choice</type><fixedValueSet><value>none</value><value>fixed</value><value>open</value><value>closed</value></fixedValueSet></validation>";
-            setting.key = "IDLookupType";
-            setting.name = "Identity Lookup Type";
-            setting.description = "Four possible values: None (anything goes), Fixed (uses the FixedId setting), Open(new names added to the store), Closed(only names in the store)";
-            setting.strValue = "Closed";
-            _settings.Add(setting);
-
-            //log service URL
-            setting = new configItem();
-            setting.validationXML = "<validation><type>file</type><mustExist>no</mustExist></validation>";
-            setting.key = "logServiceUrl";
-            setting.name = "Calculation Logging File";
-            setting.description = "The file containing the log of calculations that this calculator has performed";
-            setting.strValue = "%documents%fatiguelog.csv";
-            _settings.Add(setting);
-
-            //device ID
-            setting = new configItem();
-            setting.validationXML = "<validation><type>string</type><maxLength>25</maxLength><minLength>1</minLength><HTMLencode>no</HTMLencode></validation>";
-            setting.key = "deviceId";
-            setting.name = "Device Name";
-            setting.description = "The name of the PC, kiosk or device that the calculator is being run on";
-            setting.strValue = "TestDevice";
-            _settings.Add(setting);
-
-            //fixed ID
-            setting = new configItem();
-            setting.validationXML = "<validation><type>string</type><maxLength>25</maxLength><minLength>1</minLength><HTMLencode>no</HTMLencode></validation>";
-            setting.key = "fixedId";
-            setting.name = "Fixed Identity";
-            setting.description = "If the ID Lookup Type is Fixed, this is the identity used for all calculations.";
-            setting.strValue = "SingleUser";
-            _settings.Add(setting);
-
-            //research page
-            setting = new configItem();
-            setting.validationXML = "<validation><type>choice</type><fixedValueSet><value>shown</value><value>not shown</value></fixedValueSet></validation>";
-            setting.key = "researchPage";
-            setting.name = "Show Research Page";
-            setting.description = "Determines if the Research Page asking the user for their consent for research data is shown or not. Values can be 'shown' or 'not shown'";
-            return true;
-        }
 
         public static bool saveSettings()
         {
--- Config/FatigueCalcConfig.cs
+++ Config/FatigueCalcConfig.cs
@@ -9,6 +9,7 @@ namespace Fatigue_Calculator_Desktop.Config
     {
 
         private Dictionary<string,configItem> _itemList = new Dictionary<string,configItem>();
+
         public configItem item(string key)
         {
             configItem result = null;
@@ -16,57 +17,215 @@ namespace Fatigue_Calculator_Desktop.Config
                 return result;    
             return new configItem();
         }
-        public Dictionary<string,configItem> itemList()
+        public Dictionary<string, configItem> itemList()
         {
             return _itemList;
         }
-        private configItem _lowThreshold;
         public int lowThreshold
         {
-            get { return (int)_lowThreshold.intValue; }
-            set { _lowThreshold.intValue = value; }
+            get 
+            {
+                configItem setting;
+                if(_itemList.TryGetValue("lowThreshold", out setting))
+                {
+                    return setting.intValue;
+                }
+                // create and return the default
+                setting = setDefaultLowThreshold();
+                return setting.intValue;
+            }
+            set 
+            {
+                configItem setting;
+                if(_itemList.TryGetValue("lowThreshold", out setting))
+                    setting.intValue = value;
+                else
+                {
+                    setting = setDefaultLowThreshold();
+                    setting.intValue = value;
+                }
+            }
         }
-        private configItem _highThreshold;
+        
         public int highThreshold
         {
-            get { return _highThreshold.intValue;}
-            set { _highThreshold.intValue = value; }
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("highThreshold", out setting))
+                {
+                    return setting.intValue;
+                }
+                // create and return the default
+                setting = setDefaultHighThreshold();
+                return setting.intValue;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("highThreshold", out setting))
+                    setting.intValue = value;
+                else
+                {
+                    setting = setDefaultHighThreshold();
+                    setting.intValue = value;
+                }
+            }
         }
-        private configItem _IDLookupFile;
+        
         public string IDLookupFile
         {
-            get { return _IDLookupFile.strValue; }
-            set { _IDLookupFile.strValue = value; }
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("IDLookupFile", out setting))
+                {
+                    return setting.strValue;
+                }
+                // create and return the default
+                setting = setDefaultIDLookupFile();
+                return setting.strValue ;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("IDLookupFile", out setting))
+                    setting.strValue = value;
+                else
+                {
+                    setting = setDefaultIDLookupFile();
+                    setting.strValue = value;
+                }
+            }
         }
-        private configItem _IDLookupType;
+
         public string IDLookupType
         {
-            get{return _IDLookupType.strValue;}
-            set{_IDLookupType.strValue = value;}
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("IDLookupType", out setting))
+                {
+                    return setting.strValue;
+                }
+                // create and return the default
+                setting = setDefaultIDLookupType();
+                return setting.strValue;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("IDLookupType", out setting))
+                    setting.strValue = value;
+                else
+                {
+                    setting = setDefaultIDLookupType();
+                    setting.strValue = value;
+                }
+            }
         }
         private configItem _deviceId;
         public string deviceId
         {
-            get{return _deviceId.strValue;}
-            set{_deviceId.strValue = value;}
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("deviceId", out setting))
+                {
+                    return setting.strValue;
+                }
+                // create and return the default
+                setting = setDefaultDeviceId();
+                return setting.strValue;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("deviceId", out setting))
+                    setting.strValue = value;
+                else
+                {
+                    setting = setDefaultDeviceId();
+                    setting.strValue = value;
+                }
+            }
+
         }
-        private configItem _fixedId;
+
         public string fixedId
         {
-            get { return _fixedId.strValue; }
-            set { _fixedId.strValue = value; }
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("fixedId", out setting))
+                {
+                    return setting.strValue;
+                }
+                // create and return the default
+                setting = setDefaultFixedId();
+                return setting.strValue;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("fixedId", out setting))
+                    setting.strValue = value;
+                else
+                {
+                    setting = setDefaultFixedId();
+                    setting.strValue = value;
+                }
+            }
         }
-        private configItem _logServiceURL;
         public string logServiceUrl
         {
-            get { return _logServiceURL.strValue; }
-            set { _logServiceURL.strValue = value; }
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("logServiceUrl", out setting))
+                {
+                    return setting.strValue;
+                }
+                // create and return the default
+                setting = setDefaultLogServiceUrl();
+                return setting.strValue;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("logServiceUrl", out setting))
+                    setting.strValue = value;
+                else
+                {
+                    setting = setDefaultLogServiceUrl();
+                    setting.strValue = value;
+                }
+            }
         }
-        private configItem _researchPage;
         public string researchPage
         {
-            get { return _researchPage.strValue; }
-            set { _researchPage.strValue = value; }
+            get
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("researchPage", out setting))
+                {
+                    return setting.strValue;
+                }
+                // create and return the default
+                setting = setDefaultResearchPage();
+                return setting.strValue;
+            }
+            set
+            {
+                configItem setting;
+                if (_itemList.TryGetValue("researchPage", out setting))
+                    setting.strValue = value;
+                else
+                {
+                    setting = setDefaultResearchPage();
+                    setting.strValue = value;
+                }
+            }
         }
         /// <summary>
         /// adds a configItem to the config settings
@@ -74,58 +233,113 @@ namespace Fatigue_Calculator_Desktop.Config
         /// <param name="newItem">the configItem to add</param>
         public void Add(configItem newItem)
         {
-            // work out which config item this is
-            switch (newItem.key.ToLower())
+            // check if we've already got an item with this name
+            configItem existing;
+            if (_itemList.TryGetValue(newItem.key, out existing))
             {
-                case "lowthreshold":
-                    {
-                        _lowThreshold = newItem;
-                        break;
-                    }
-                case "highthreshold":
-                    {
-                        _highThreshold = newItem;
-                        break;
-                    }
-                case "idlookupfile":
-                    {
-                        _IDLookupFile = newItem;
-                        break;
-                    }
-                case "idlookuptype":
-                    {
-                        _IDLookupType = newItem;
-                        break;
-                    }
-                case "deviceid":
-                    {
-                        _deviceId = newItem;
-                        break;
-                    }
-                case "fixedid":
-                    {
-                        _fixedId = newItem;
-                        break;
-                    }
-                case "logserviceurl":
-                    {
-                        _logServiceURL = newItem;
-                        break;
-                    }
-                case "researchpage":
-                    {
-                        _researchPage = newItem;
-                        break;
-                    }
-                default:
-                    {
-                        //throw new Exception("unknown config handle");
-                        // just add it to the list but don't assign a handle
-                        break;
-                    }
+                // remove the existing item so we can add the new one
+                _itemList.Remove(existing.key);
             }
+            // add the new one
             _itemList.Add(newItem.key, newItem);
-            return;
         }
+
+        private configItem setDefaultLowThreshold()
+        {
+            //low threshold
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>int</type><min>1</min><max>24</max></validation>";
+            setting.key = "lowThreshold";
+            setting.name = "Low Threshold";
+            setting.description = "The calculation score that separates Moderate fatigue risk from High fatigue risk";
+            setting.intValue = 6;
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultHighThreshold()
+        {
+            //high threshold
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>int</type><min>2</min><max>24</max></validation>";
+            setting.key = "highThreshold";
+            setting.name = "High Threshold";
+            setting.description = "The calculation score that separates High fatigue risk from Extreme fatigue risk";
+            setting.intValue = 12;
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultIDLookupFile()
+        {
+            //ID Lookup File
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>file</type><mustExist>no</mustExist></validation>";
+            setting.key = "IDLookupFile";
+            setting.name = "Identity Lookup File";
+            setting.description = "The file containing the list of valid identities for this calculator";
+            setting.strValue = "%appdata%/fatigue calculator/Identities.csv";
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultIDLookupType()
+        {
+            //ID Lookup Type
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>choice</type><fixedValueSet><value>none</value><value>fixed</value><value>open</value><value>closed</value></fixedValueSet></validation>";
+            setting.key = "IDLookupType";
+            setting.name = "Identity Lookup Type";
+            setting.description = "Four possible values: None (anything goes), Fixed (uses the FixedId setting), Open(new names added to the store), Closed(only names in the store)";
+            setting.strValue = "Closed";
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultLogServiceUrl()
+        {
+            //log service URL
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>file</type><mustExist>no</mustExist></validation>";
+            setting.key = "logServiceUrl";
+            setting.name = "Calculation Logging File";
+            setting.description = "The file containing the log of calculations that this calculator has performed";
+            setting.strValue = "%documents%fatiguelog.csv";
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultDeviceId()
+        {
+            //device ID
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>string</type><maxLength>25</maxLength><minLength>1</minLength><HTMLencode>no</HTMLencode></validation>";
+            setting.key = "deviceId";
+            setting.name = "Device Name";
+            setting.description = "The name of the PC, kiosk or device that the calculator is being run on";
+            setting.strValue = "TestDevice";
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultFixedId()
+        {
+            //fixed ID
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>string</type><maxLength>25</maxLength><minLength>1</minLength><HTMLencode>no</HTMLencode></validation>";
+            setting.key = "fixedId";
+            setting.name = "Fixed Identity";
+            setting.description = "If the ID Lookup Type is Fixed, this is the identity used for all calculations.";
+            setting.strValue = "SingleUser";
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+        private configItem setDefaultResearchPage()
+        {
+            //research page
+            configItem setting = new configItem();
+            setting.validationXML = "<validation><type>choice</type><fixedValueSet><value>shown</value><value>not shown</value></fixedValueSet></validation>";
+            setting.key = "researchPage";
+            setting.name = "Show Research Page";
+            setting.description = "Determines if the Research Page asking the user for their consent for research data is shown or not. Values can be 'shown' or 'not shown'";
+            setting.strValue = "shown";
+            _itemList.Add(setting.key, setting);
+            return setting;
+        }
+
     }
 }
